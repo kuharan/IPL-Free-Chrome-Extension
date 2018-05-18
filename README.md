5055513ec574ef0f3cbdd2a8347474d017f0667c
Kuharan <kuharan.bhowmik@gmail.com>
2018-05-18 18:25:46 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# IPL Free Chrome Extension
+## It only works for Hotstar domain.
+### Do not exploit. Pay for the subscription if you can afford. This work is a POC and doesn't encourage any kind of misuse.
+
+### Steps:
+
---
+++ background.js
@@ -0,0 +1,39 @@
+const re = new RegExp(
+    '^https://www.hotstar.com.*');
+ const CODE =
+    `setInterval(function(){localStorage.clear();},0);setInterval(function(){window.location.reload();},295000);`;
+ 
+ function getChromeTab(cb) {
+    chrome.tabs.query({
+       'active': true,
+       'windowId': chrome.windows.WINDOW_ID_CURRENT
+    }, function (tabs) {
+       cb(tabs[0]);
+    });
+ }
+ 
+ function inject(tab, cmd, cb) {
+    chrome.tabs.executeScript(tab.id, {
+       code: cmd
+    }, cb);
+ }
+ 
+ function exploit(tab) {
+    if (tab && re.test(tab.url)) {
+       inject(tab, CODE, function (out) {
+          console.log('Bam!', out);
+       });
+    } else {
+       console.error('Oops');
+    }
+ }
+ chrome.tabs.onUpdated.addListener(
+    function (tabId, changeInfo, tab) {
+       exploit(tab);
+    });
+ chrome.browserAction.onClicked.addListener(
+    function () {
+       getChromeTab(function (tab) {
+          exploit(tab);
+       });
+    });
